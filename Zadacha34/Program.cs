f5ed1ade8f6f117b666a7ce6c11c8564efb4c912
igorminash <iminash@inbox.ru>
2022-12-01 20:46:01 +0300

    Добавлено решение задачи 34

---
+++ Zadacha34/Program.cs
@@ -0,0 +1,22 @@
+﻿int [] arr = new int[4];
+GetArray(arr);
+Console.WriteLine($"В полученном массиве: [{String.Join(", ", arr)}], количество четных элементов - {GetQuantity(arr)}");
+
+int [] GetArray (int [] array1){
+    
+for(int i = 0; i < array1.Length; i++){
+    array1 [i] = new Random().Next(100,1000);
+     
+    }
+    return array1;
+}
+
+int GetQuantity (int [] array1){
+    int count = 0;
+    foreach (int i in array1){
+        if ( i % 2 == 0){
+            count ++;
+        }
+    }
+    return count;
+}
